e31ca611cd0da170be9c2607a5f605c77ffaa687
imalexianne <imanirax@yahoo.fr>
2019-02-11 13:03:29 +0200

    Add a save method and a save test

--- user.py
+++ user.py
@@ -1,15 +1,4 @@
-class User:
-    """
-    Class that generates new instances of users
-    """
-    contact_list=[]
-    def_init_(self, username, password):
-    """
-    _init_method that helps us define properties for our objects.
-    """
-        self.username=username
-        self.password=password
-  
+
 
 
 # class Credential:
@@ -17,3 +6,25 @@ class User:
 #   Class that generates new instances of credentials
 #   """
   
+class User:
+    """
+    Class that generates new instances of contacts.
+    """
+
+    user_list = [] # Empty user list
+
+    def __init__(self,user_name,password):
+        """
+        __init__ method that helps us define properties for our objects.
+        """
+        self.user_name = user_name
+        self.password = password
+       
+
+    def save_user(self):
+
+        '''
+        save_user method saves user objects into user_list
+        '''
+
+        User.user_list.append(self)
--- user_test.py
+++ user_test.py
@@ -12,13 +12,22 @@ class TestUser(unittest.TestCase):
         '''
         Set up method to run before each test cases.
         '''
-        self.new_user=User("username","password")
+        self.new_user=User("user_name","password")
 
     def test_init(self):
         '''
         test_init test case to test if the object is initialized properly
         '''
-        self.assertEqual(self.new_user.username, "username")
+        self.assertEqual(self.new_user.user_name, "user_name")
         self.assertEqual(self.new_user.password, "password")
+
+    def test_save_user(self):
+        '''
+        test_save_user test case to test if the user object is saved into
+         the user list
+        '''
+        self.new_user.save_user() # saving the new contact
+        self.assertEqual(len(User.user_list),1)
+
 if __name__ == '__main__':
     unittest.main()
